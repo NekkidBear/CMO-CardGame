a4b14ca9ec84460b0166171740f0f1281113d2cf
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-04-28 15:28:15 -0500

    testing css rendering

--- src/Card/CardV2.jsx
+++ src/Card/CardV2.jsx
@@ -1,10 +1,21 @@
 import React, {useState} from 'react';
 import './css-cards/Card.css'; // Import the CSS file with the provided styles
 import backImage from '../assets/back_image/card-back-black.png';
-import { cups, coins, staves, swords, terrapins, octograms, elephants, crowns,king, queen, jack} from '../assets/suit_icons'
+import cups from '../assets/suit_icons/cups.svg';
+import coins from '../assets/suit_icons/Coins.svg';
+import staves from '../assets/suit_icons/staves.svg';
+import swords from '../assets/suit_icons/Swords.svg'
+import terrapins  from '../assets/suit_icons/terrapins.svg';
+import octograms from '../assets/suit_icons/octagram.svg';
+import elephants from '../assets/suit_icons/elephants.svg';
+import crowns from '../assets/suit_icons/Crowns.svg';
+import king  from '../assets/suit_icons/king.svg';
+import queen  from '../assets/suit_icons/queen.svg';
+import jack from '../assets/suit_icons/jack.svg';
+
 
 function Card({ rank, suit, value }) {
-  let [isFaceDown, setIsFaceDown] = useState(true)
+  let [isFaceDown, setIsFaceDown] = useState(false)
   const isRoyalRank = ['A', 'J', 'Q', 'K'].includes(rank);
   const pipImages = {
     'cups': cups,
@@ -25,7 +36,7 @@ function Card({ rank, suit, value }) {
   const renderPips = () => {
     const pips = [];
     for (let i = 0; i < pipCount; i++) {
-      pips.push(<div key={i} className="pip" style={{ backgroundImage: `url(${pipImages[suit]}.svg)` }} />);
+      pips.push(<div key={i} className="pip" style={{ backgroundImage: `url(${pipImages[suit]})` }} />);
     }
     return pips;
   };
